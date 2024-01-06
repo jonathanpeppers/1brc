d3e3a44ea0822939d04b334c7574e02a6ffeaa17
Jonathan Peppers <jonathan.peppers@microsoft.com>
2024-01-05 22:47:02 -0600

    Looked cute, might delete

--- dotnet-src/BillionRowChallenge/Program.cs
+++ dotnet-src/BillionRowChallenge/Program.cs
@@ -15,7 +15,7 @@ while (!reader.EndOfStream)
     line = reader.ReadLine()!;
     index = line.IndexOf(';');
     name = line[..index];
-    value = float.Parse(line[(index + 1)..]);
+    value = FastParse(line.AsSpan()[(index + 1)..]);
     if (measurements.TryGetValue(name, out var m))
     {
         m.Add(value);
@@ -41,6 +41,31 @@ foreach (var measurement in measurements.OrderBy(x => x.Key))
     Console.Write($"{measurement.Key}={measurement.Value.Min:0.#}/{measurement.Value.Mean:0.#}/{measurement.Value.Max:0.#}");
 }
 Console.WriteLine('}');
+
+static float FastParse(ReadOnlySpan<char> text)
+{
+    float value = 0;
+    int position = 0;
+    bool negative = false;
+    char c;
+    if (text[0] == '-')
+    {
+        negative = true;
+        position++;
+    }
+    while (position < text.Length)
+    {
+        c = text[position];
+        if (c == '.')
+        {
+            break;
+        }
+        value *= 10;
+        value += c - '0';
+        position++;
+    }
+    return negative ? -value : value;
+}
 
 class Measurement
 {
